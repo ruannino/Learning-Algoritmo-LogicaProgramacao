cd051e2299c1ff6d2628cdf0e83867f1b50cdbf5
Ruannino <ruanfariasdc@hotmail.com>
2022-11-19 21:52:55 -0300

    19.11.2022

---
+++ fundamentos/Exercícios/Q20.js
@@ -0,0 +1,8 @@
+// Faça uma função que recebe 
+// a base e a altura de um 
+// triângulo e retorne a área 
+// desse triângulo. A precisão 
+// deverá ser de duas casas 
+// decimais, arredondando se 
+// necessário.
+
