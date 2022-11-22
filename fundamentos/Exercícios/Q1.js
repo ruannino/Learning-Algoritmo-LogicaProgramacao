e71ae2db712ed100be1d71cb2914a064e287c4a5
Ruannino <ruanfariasdc@hotmail.com>
2022-11-22 16:21:12 -0300

    Change caracteres

--- fundamentos/Exercícios/Q1.js
+++
@@ -1,9 +0,0 @@
-// Crie uma função que retorna a string "Olá, " 
-// concatenada com um argumento text (a ser passado para a 
-// função) e com ponto de exclamação "!" no final.
-
-function cumprimentar(nome) {
-    return "Olá, " + nome + "!"
-}
-
-console.log(cumprimentar("Ruannino"));
