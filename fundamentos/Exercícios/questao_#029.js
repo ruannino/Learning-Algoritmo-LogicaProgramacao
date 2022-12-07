8b545590af33536bbb852cab3539b8384952f30c
Ruannino <ruanfariasdc@hotmail.com>
2022-12-06 23:27:55 -0300

    06.12.2022

--- fundamentos/Exercícios/questao_#029.js
+++ fundamentos/Exercícios/questao_#029.js
@@ -3,4 +3,25 @@
 // o segundo maior número presente 
 // nesse array.
 
-function 
+function segundoMaior(numeros) {
+    let indiceDoMaior = 0
+    let segundoMaior
+
+    numeros.forEach( (numero, indice) => {
+        if(numero > numeros[indiceDoMaior])
+            indiceDoMaior = indice
+    })
+
+    numeros.splice(indiceDoMaior, 1)
+    segundoMaior = numeros[0]
+
+    numeros.forEach(numero => {
+        if(numero > segundoMaior)
+            segundoMaior = numero
+    })
+
+    return segundoMaior
+}
+
+console.log(segundoMaior([15, 17, 3, 8]))
+console.log(segundoMaior([2083719087, 235, 123123, 321]))
